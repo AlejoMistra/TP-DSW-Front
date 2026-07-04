c5634382acc6bffb4141ef2d1488b78dbe2181f1
AlejoMistra <mistralettialejo@gmail.com>
2026-07-04 19:33:46 -0300

    refactor: simplify main.tsx by removing router and using App component

--- gym-frontend/src/main.tsx
+++ gym-frontend/src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import { router } from './routes/router.tsx'
-import { RouterProvider } from 'react-router-dom'
+import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
