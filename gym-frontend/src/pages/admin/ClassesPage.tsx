90eb56cb8d346637c600798386db2cd86d4f54f7
AlejoMistra <mistralettialejo@gmail.com>
2026-07-12 00:06:07 -0300

    feat: refactor renaming ClasesPage and SociosPage; update routing to reflect changes

---
+++ gym-frontend/src/pages/admin/ClassesPage.tsx
@@ -0,0 +1,8 @@
+export default function AdminClassesPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Clases</h1>
+      <p>Esta es la página de clases para el administrativo</p>
+    </div>
+  )
+}
